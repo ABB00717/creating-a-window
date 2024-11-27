fd90fa4ce981c6256bd30f87801d7dbf44b7ba9c
ABB00717 <eric94528@gmail.com>
2024-11-27 21:11:18 +0800

    Hello Window

--- creating-a-window/main.cpp
+++ creating-a-window/main.cpp
@@ -5,6 +5,9 @@
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
+void frameBufferSizeCallback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
 int main() {
   // Init GLFW
   glfwInit();
@@ -20,7 +23,29 @@ int main() {
   // Init Glad
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) return false;
 
-  glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+  glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT); // 設定視口大小
+  glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback); // 設定視窗大小改變時的callback
   while (!glfwWindowShouldClose(window)) {
+    processInput(window); // 檢查是否按下ESC
+
+    // 渲染指令
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // 設定清除顏色
+    glClear(GL_COLOR_BUFFER_BIT); // 清除顏色緩衝
+
+    glfwSwapBuffers(window); // 雙緩衝區交換
+    glfwPollEvents(); // 檢查有沒有觸發事件
+  }
+
+  glfwTerminate();
+  return 0;
+}
+
+void frameBufferSizeCallback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE)==GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
   }
 }

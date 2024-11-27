dcd91c2c90890e4fb2cee70464e9cc3a040ff726
ABB00717 <eric94528@gmail.com>
2024-11-27 21:42:07 +0800

    繪製兩個三角形

--- creating-a-window/main.cpp
+++ creating-a-window/main.cpp
@@ -4,10 +4,15 @@
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
-const float vertices[] = {
-  -0.5f, -0.5f, 0.0f,
-   0.5f, -0.5f, 0.0f,
-   0.0f,  0.5f, 0.0f
+float vertices[] = {
+    0.5f, 0.5f, 0.0f,   // 右上角
+    0.5f, -0.5f, 0.0f,  // 右下角
+    -0.5f, -0.5f, 0.0f, // 左下角
+    -0.5f, 0.5f, 0.0f   // 左上角
+};
+unsigned int indices[] = {
+  0, 1, 3,
+  1, 2, 3
 };
 const char* vertexShaderSource = 
 "#version 330 core\n"
@@ -79,21 +84,23 @@ int main() {
   glDeleteShader(vertexShader); // 刪除已經連結到程序的頂點著色器
   glDeleteShader(fragmentShader); // 刪除已經連結到程序的片段著色器
 
-  // 綁定 VBO, VAO
-  unsigned int VBO, VAO;
+  // 綁定 VBO, VAO, EBO
+  unsigned int VBO, VAO, EBO;
   glGenVertexArrays(1, &VAO);
-  glGenBuffers(1, &VBO);
   glBindVertexArray(VAO);
+  glGenBuffers(1, &VBO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glGenBuffers(1, &EBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
   // 告訴OpenGL如何解析頂點數據
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
 
-  // 解綁 VBO, VAO
+  // 解綁 VBO, VAO, EBO
   glBindBuffer(GL_ARRAY_BUFFER, 0);
-  glBindVertexArray(0);
 
   glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT); // 設定視口大小
   glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback); // 設定視窗大小改變時的callback
@@ -101,12 +108,12 @@ int main() {
     processInput(window); // 檢查是否按下ESC
 
     // 渲染指令
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // 線框模式
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // 設定清除顏色
     glClear(GL_COLOR_BUFFER_BIT); // 清除顏色緩衝
     glUseProgram(shaderProgram);
-    glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-    glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     glfwSwapBuffers(window); // 雙緩衝區交換
     glfwPollEvents(); // 檢查有沒有觸發事件

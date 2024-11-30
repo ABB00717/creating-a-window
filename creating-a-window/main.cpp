deb794d33cbc7f35bc9c7de74f66269d75d0b9e7
ABB00717 <eric94528@gmail.com>
2024-11-30 17:47:55 +0800

    炫泡迷幻三角形

--- creating-a-window/main.cpp
+++ creating-a-window/main.cpp
@@ -5,29 +5,29 @@
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 float vertices[] = {
-    0.5f, 0.5f, 0.0f,   // 右上角
-    0.5f, -0.5f, 0.0f,  // 右下角
-    -0.5f, -0.5f, 0.0f, // 左下角
-    -0.5f, 0.5f, 0.0f   // 左上角
+  // 位置              // 顏色
+   0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // 右下
+  -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // 左下
+   0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // 頂部
 };
 unsigned int indices[] = {
-  0, 1, 3,
-  1, 2, 3
+  0, 1, 2,
 };
 const char* vertexShaderSource =
 "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
-"out vec4 vertexColor;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main() {\n"
 "  gl_Position = vec4(aPos, 1.0);\n"
-"  vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+"  ourColor = aColor;\n"
 "}\0";
 const char* fragmentShaderSource =
 "#version 330 core\n"
 "out vec4 FragColor;\n"
-"uniform vec4 ourColor;\n"
+"in vec3 ourColor;\n"
 "void main() {\n"
-"  FragColor = ourColor;\n"
+"  FragColor = vec4(ourColor, 1.0);\n"
 "}\0";
 
 void frameBufferSizeCallback(GLFWwindow* window, int width, int height);
@@ -99,8 +99,12 @@ int main() {
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
   // 告訴OpenGL如何解析頂點數據
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+  // 位置屬性
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
+  // 顏色屬性
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3*sizeof(float)));
+  glEnableVertexAttribArray(1);
 
   // 解綁 VBO, VAO, EBO
   glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -111,19 +115,13 @@ int main() {
     processInput(window); // 檢查是否按下ESC
 
     // 渲染指令
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // 線框模式
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // 線框模式
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // 設定清除顏色
     glClear(GL_COLOR_BUFFER_BIT); // 清除顏色緩衝
 
     // 啟動著色器
     glUseProgram(shaderProgram);
 
-    // 更新uniform顏色
-    float timeValue = glfwGetTime();
-    float greenValue = sin(timeValue)/2.0f+0.5f;
-    int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-    glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
-    
     // 繪製物件
     glBindVertexArray(VAO);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
